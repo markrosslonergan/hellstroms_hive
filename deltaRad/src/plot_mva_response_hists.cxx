8a9d8fac1bdbb77bc2ecb1848d2651966c264e7a
markrosslonergan <markrosslonergan@gmail.com>
2017-11-13 18:36:22 -0500

    mend

--- deltaRad/src/plot_mva_response_hists.cxx
+++ deltaRad/src/plot_mva_response_hists.cxx
@@ -50,7 +50,9 @@ void draw_mva_response_hists(std::string const & ifile_path,
 					<< "Different number of bins: " << hist->GetNbinsX() << " " << bins << "\n";
 			}
 		}
-		if(method=="BDTG") canvas->SetLogy();
+		if(method=="BDTG") {
+			//canvas->SetLogy();
+		}
 
 		hist->Scale(1./hist->Integral(1, hist->GetNbinsX()));
 		hist->SetLineColor(i+1);
