c41042ae8f4baf1f30bcb8718771aee5c6f570e2
Robert Murrells <robertmurrells@gmail.com>
2018-03-07 05:52:30 -0600

    grid test file now working

--- VertexReconstruction/other/grid/rungrid_test.sh
+++ VertexReconstruction/other/grid/rungrid_test.sh
@@ -5,24 +5,57 @@ umask +w
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 
 
-log=log_${PROCESS}.txt 
+log=log${PROCESS}.txt 
 echo "Running on "$HOSTNAME > $log
 echo "CLUSTER " ${CLUSTER} >> $log
 echo "PROCESS " ${PROCESS} >> $log
 echo >> $log
 
 
-setup ifdhc v2_0_1 -q e10:p2711:prof
-
-
 RES=/pnfs/uboone/resilient/users/rmurrell
+SCRATCH=/pnfs/uboone/scratch/users/rmurrell
+VIN=vertex_quality_input
+VOUT=vertex_quality_output
+
 EXEC=RunVertexQuality
-IN=vertex_quality_input
-OUT=vertex_quality_output
+INPUT_PERM_FILE=root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/uboone/scratch/users/rmurrell/vertex_quality_input/permutations_$PROCESS.root
+INPUT_FILE=root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/uboone/scratch/users/rmurrell/vertex_quality_input/FillLightEvent_1.root
 
 
-#echo ifdh cp -D $RES/$IN/$EXEC $CONDIR_DIR_INPUT >> $log
-#ifdh cp -D $RES/$IN/$EXEC $CONDIR_DIR_INPUT >> $log 2>&1
+echo setup uboonecode v06_26_01_10 -q e10:prof >>$log
+setup uboonecode v06_26_01_10 -q e10:prof >> $log 2>&1
+echo >> $log
 
-echo ifdh cp -D $PWD/$log $SCRATCH >> $log
-ifdh cp -D $PWD/$log $SCRATCH
+echo ifdh cp -D $RES/$VIN/$EXEC $CONDOR_DIR_INPUT >> $log
+ifdh cp -D $RES/$VIN/$EXEC $CONDOR_DIR_INPUT >> $log 2>&1
+echo >> $log
+
+echo chmod 777 $CONDOR_DIR_INPUT/$EXEC >> $log
+chmod 777 $CONDOR_DIR_INPUT/$EXEC >> $log 2>&1
+echo >> $log
+
+echo $CONDOR_DIR_INPUT/$EXEC $INPUT_PERM_FILE $INPUT_FILE >> $log
+$CONDOR_DIR_INPUT/$EXEC $INPUT_PERM_FILE $INPUT_FILE >> $log 2>&1
+echo >> $log
+
+
+echo ifdh mkdir $SCRATCH/$VOUT/$PROCESS >> $log
+ifdh mkdir $SCRATCH/$VOUT/$PROCESS >> $log 2>&1
+echo >> $log
+
+ofile=$PWD/$EXEC.root
+echo Output file: $ofile >> $log
+echo >> $log
+ls $ofile >> $log 2>&1
+if [ $? -eq 0 ];
+then
+    chmod 777 $ofile
+    echo ifdh cp -D $ofile $SCRATCH/$OUT/$PROCESS >> $log
+    ifdh cp -D $ofile $SCRATCH/$OUT/$PROCESS >> $log 2>&1
+else 
+    echo $ofile not found >> $log
+fi
+echo >> $log
+
+echo ifdh cp -D $PWD/$log $SCRATCH/$VOUT >> $log
+ifdh cp -D $PWD/$log $SCRATCH/$VOUT
