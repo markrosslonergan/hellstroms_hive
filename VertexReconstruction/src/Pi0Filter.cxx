a575e535d6300cadf6a992a6c687ab788e4968b2
Bobby <bobby@bobbys-mbp.home>
2018-07-16 19:03:39 +0100

    Updated Pi0Filter

--- VertexReconstruction/src/Pi0Filter.cxx
+++ VertexReconstruction/src/Pi0Filter.cxx
@@ -12,9 +12,23 @@ bool Pi0Filter::Run() {
 
   std::vector<int> const & ccnc = *fstorage->fccnc;
 
-  bool pass = true;
+  if(ccnc.front() == 0) return false;
 
-  if(ccnc.front() == 0) pass = false;
+  std::vector<int> const & reco_shower_largest_mc_type = *fstorage->freco_shower_largest_mc_type;
+  std::vector<int> const & reco_shower_largest_mc_index = *fstorage->freco_shower_largest_mc_index;
+  auto & shower_indices = fstorage->GetShowerIndices("pandoraNu");
+  
+  std::vector<int> const & mcshower_PdgCode = *fstorage->fmcshower_PdgCode;
+  std::vector<int> const & mcshower_MotherPdgCode = *fstorage->fmcshower_MotherPdgCode;
+
+  bool pass = false;
+
+  for(size_t s = shower_indices.first; s < shower_indices.second; ++s) {
+    if(reco_shower_largest_mc_type.at(s) != 1) continue;
+    size_t const mcs = reco_shower_largest_mc_index.at(s);
+    if(mcshower_PdgCode.at(mcs) != 22 || mcshower_MotherPdgCode.at(mcs) != 111) continue;
+    pass = true;
+  }
   
   return pass;
 
