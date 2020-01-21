cc6541bbb2e68def19854bfc5910beb3b97556df
Kathryn Sutton <ksutton@uboonegpvm01.fnal.gov>
2020-01-21 17:00:04 -0600

    fixed label datamc

--- hive/src/bdt_datamc.cxx
+++ hive/src/bdt_datamc.cxx
@@ -317,7 +317,7 @@ int bdt_datamc::plotStacks(TFile *ftest, std::vector<bdt_variable> vars, std::ve
 
     ftest->cd();
 
-    std::vector<std::string> stage_names = {"Topological Selection","Pre-Selection Cuts","Cosmic BDT Cut","BNB BDT cut","NCPi0 BDT Cut","NUE BDT Cut","tmp"};
+    std::vector<std::string> stage_names = {"Topological Selection","Pre-Selection Cuts","Cosmic BDT Cut","BNB BDT cut","NCPi0 BDT Cut","NUE BDT Cut","SSV"};
     //Loop over all stages
 
     int s_min = 0;
