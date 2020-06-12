1141c81db098d9936926b3860aaca1c2c9401d44
Kathryn Sutton <ksutton@uboonegpvm01.fnal.gov>
2020-06-12 12:50:44 -0500

    reverted plotting

--- hive/src/bdt_datamc.cxx
+++ hive/src/bdt_datamc.cxx
@@ -482,8 +482,8 @@ int bdt_datamc::plotStacks(TFile *ftest, std::vector<bdt_variable> vars, std::ve
                     max_modifier = (stack_mode ? 2.0 : 1.85);
                 }
             }
-            if(s>4){
-                max_modifier = 3.0;
+            if(s>5){
+                max_modifier = 2.75;
             }
 
 
