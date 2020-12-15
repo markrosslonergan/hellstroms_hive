ec4289c2e0e899144653da056539dabb45b961e5
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2020-12-15 14:00:01 -0600

    Some BNB Merging

---
+++ hive/other/BNBCommonOpticalSplitting/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#hadd  /uboone/data/users/markross/Mar2020/bnb_bdt_testing_sample_extra_v43.5.root /uboone/data/users/markross/Mar2020/bnb_bdt_testing_sample_v33.3.uniq.root  /uboone/data/users/markross/Mar2020/bnb_overlay_combined_run13_Extra_v43.5.root
+
+#hadd  /uboone/data/users/markross/Mar2020/bnb_overlay_run1_v43.5_extra.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/bnb_overlay_run1_v33.3.uniq.spluff.test.root  /uboone/data/users/markross/Mar2020/bnb_overlay_run1_Extra_v43.5.root
+
+#hadd  /uboone/data/users/markross/Mar2020/bnb_overlay_run3_v43.5_extra.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/bnb_overlay_combined_run3_v33.3.uniq.spluff.test.root  /uboone/data/users/markross/Mar2020/bnb_overlay_run3_Extra_v43.5.root
+
+#hadd /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_bdt_testing_sample_extra_v43.5.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_bdt_testing_sample_v33.3.uniq.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_combined_run13_Extra_v43.5.root
+
+#hadd /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_run1_v43.5_extra.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_run1_v33.3.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_run1_Extra_v43.5.root
+
+hadd /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_run3_v43.5_extra.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_combined_run3_v33.3.uniq.spluff.test.root /uboone/data/users/markross/Mar2020/COFredo/OpticalMatch_bnb_overlay_run3_Extra_v43.5.root
+
