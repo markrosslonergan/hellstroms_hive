f91706aa7e840b2a415014c2e3232e3816012a95
Kathryn Sutton <ksutton@uboonegpvm01.fnal.gov>
2021-02-16 13:41:58 -0600

    typo fix in run 1 1g1p xml

--- hive/other/run1g0p_technote_2_15_21_Run1.sh
+++ hive/other/run1g0p_technote_2_15_21_Run1.sh
@@ -16,25 +16,25 @@ XML13="/uboone/app/users/ksutton/hellstroms_hive_2.5_sl7/hellstroms_hive/hive/xm
 
 
 #stage0 + 1
-#./../../bin/hive --makefluxcovar $FXXML1 -x $XML -s 1 -g8
-#./../../bin/hive -o datamc  -x $XML -s 0 -g8 --systematics "flux_fracfixed"
-#./../../bin/hive -o datamc  -x $XML -s 1 -g8 --systematics "flux_fracfixed"
+./../../bin/hive --makefluxcovar $FXXML1 -x $XML -s 1 -g8
+./../../bin/hive -o datamc  -x $XML -s 0 -g8 --systematics "flux_fracfixed"
+./../../bin/hive -o datamc  -x $XML -s 1 -g8 --systematics "flux_fracfixed"
 
 #stage 1
 ./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g12
 ./../../bin/hive -o datamc  -x $XML -s 1 -g12 --systematics "flux_fracfixed"
 
 #Single BDT Cuts
-#./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g13
-#./../../bin/hive -o datamc  -x $XML -s 1 -g13 --systematics "flux_fracfixed"
+./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g13
+./../../bin/hive -o datamc  -x $XML -s 1 -g13 --systematics "flux_fracfixed"
 
 #BDT Scores
 ./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g6
 ./../../bin/hive -o datamc  -x $XML -s 1 -g6 --systematics "flux_fracfixed"
 
 #stage 4 datamc + stack
-#./../../bin/hive --makefluxcovar $FXXML4 -x $XML -s 4 -g11
-#./../../bin/hive -o datamc  -x $XML -s 4 -g11 --systematics "flux_fracfixed"
+./../../bin/hive --makefluxcovar $FXXML4 -x $XML -s 4 -g11
+./../../bin/hive -o datamc  -x $XML -s 4 -g11 --systematics "flux_fracfixed"
 
 ./../../bin/hive --makefluxcovar $FXXML4 -x $XML13 -s 4 -g11
 ./../../bin/hive -o stack -x $XML13 -s 4 -g11 --systematics "flux_fracfixed" --pot 6.91e20
