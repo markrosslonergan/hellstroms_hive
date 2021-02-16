21ef0cdbdaa2cc8b7e01c0291d63c9370d869679
Kathryn Sutton <ksutton@uboonegpvm01.fnal.gov>
2021-02-16 09:44:21 -0600

    this one doesn't work for combined xmls

--- hive/other/run1g0p_technote_2_15_21_Run1.sh
+++ hive/other/run1g0p_technote_2_15_21_Run1.sh
@@ -16,13 +16,13 @@ XML13="/uboone/app/users/ksutton/hellstroms_hive_2.5_sl7/hellstroms_hive/hive/xm
 
 
 #stage0 + 1
-./../../bin/hive --makefluxcovar $FXXML1 -x $XML -s 1 -g8
-./../../bin/hive -o datamc  -x $XML -s 0 -g8 --systematics "flux_fracfixed"
-./../../bin/hive -o datamc  -x $XML -s 1 -g8 --systematics "flux_fracfixed"
+#./../../bin/hive --makefluxcovar $FXXML1 -x $XML -s 1 -g8
+#./../../bin/hive -o datamc  -x $XML -s 0 -g8 --systematics "flux_fracfixed"
+#./../../bin/hive -o datamc  -x $XML -s 1 -g8 --systematics "flux_fracfixed"
 
 #stage 1
-./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g12
-./../../bin/hive -o datamc  -x $XML -s 1 -g12 --systematics "flux_fracfixed"
+#./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g12
+#./../../bin/hive -o datamc  -x $XML -s 1 -g12 --systematics "flux_fracfixed"
 
 #Single BDT Cuts
 #./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g13
@@ -36,6 +36,6 @@ XML13="/uboone/app/users/ksutton/hellstroms_hive_2.5_sl7/hellstroms_hive/hive/xm
 #./../../bin/hive --makefluxcovar $FXXML4 -x $XML -s 4 -g11
 #./../../bin/hive -o datamc  -x $XML -s 4 -g11 --systematics "flux_fracfixed"
 
-#./../../bin/hive --makefluxcovar $FXXML4 -x $XML13 -s 4 -g11
-#./../../bin/hive -o stack -x $XML13 -s 4 -g11 --systematics "flux_fracfixed" --pot 6.91e20
+./../../bin/hive --makefluxcovar $FXXML4 -x $XML13 -s 4 -g11
+./../../bin/hive -o stack -x $XML13 -s 4 -g11 --systematics "flux_fracfixed" --pot 6.91e20
 
--- hive/other/run1g1p_technote_2_15_21_Run1.sh
+++ hive/other/run1g1p_technote_2_15_21_Run1.sh
@@ -41,13 +41,13 @@ NUM=2212
 ## in order to run a specific group of plots you can comment sections in and out
 
 #stage0 + 1
-./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g330
-./../../bin/hive -o datamc  -x $XML -s 0 -g330 --systematics "flux_fracfixed"
-./../../bin/hive -o datamc  -x $XML -s 1 -g330 --systematics "flux_fracfixed"
+#./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g330
+#./../../bin/hive -o datamc  -x $XML -s 0 -g330 --systematics "flux_fracfixed"
+#./../../bin/hive -o datamc  -x $XML -s 1 -g330 --systematics "flux_fracfixed"
 
 #stage 1
-./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g331
-./../../bin/hive -o datamc  -x $XML -s 1 -g331 --systematics "flux_fracfixed"
+#./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g331
+#./../../bin/hive -o datamc  -x $XML -s 1 -g331 --systematics "flux_fracfixed"
 
 #Single BDT Cuts
 #./../../bin/hive  --makefluxcovar $FXXML1 -x $XML -s 1 -g335
@@ -85,6 +85,7 @@ NUM=2212
 #stage 6
 #./../../bin/hive  --makefluxcovar $FXXML6 -x $XML -s 6 -g332
 #./../../bin/hive -o datamc  -x $XML -s 6 -g332 --systematics "flux_fracfixed"
-#./../../bin/hive  --makefluxcovar $FXXML6 -x $XML13 -s 6 -g332
-#./../../bin/hive -o stack -x $XML13 -s 6 -g332 --systematics "flux_fracfixed" --pot 6.91e20
+
+./../../bin/hive  --makefluxcovar $FXXML6 -x $XML13 -s 6 -g332
+./../../bin/hive -o stack -x $XML13 -s 6 -g332 --systematics "flux_fracfixed" --pot 6.91e20
 
