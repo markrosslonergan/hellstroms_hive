4827f35c2f86df9bb5ff7b72594a997b33a3b56d
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2022-11-21 16:10:38 -0600

    Fixed TH2D typo in getTH2

--- hive/inc/bdt_file.h
+++ hive/inc/bdt_file.h
@@ -314,7 +314,7 @@ struct bdt_file{
 
 	/* Grab 2D histogram of variable varx and vary, with provided cuts applied 
  	 */
-        TH2* getTH2(bdt_variable varx, bdt_variable vary, std::string cuts, std::string nam, double plot_POT);
+        TH2D* getTH2(bdt_variable varx, bdt_variable vary, std::string cuts, std::string nam, double plot_POT);
 
         std::vector<TH1*> getRecoMCTH1(bdt_variable var, std::string cuts, std::string nam, double plot_POT);
         std::vector<TH1*> getRecoMCTH1(bdt_variable var, std::string cuts, std::string nam, double plot_POT, int rebin);
