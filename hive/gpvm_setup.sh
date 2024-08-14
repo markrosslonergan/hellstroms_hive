a8c23640fa004ba8dadbc2b6305f0ea91a1b66d5
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2024-08-14 16:16:27 -0500

    Fixed setup script and CMAKE for boost on new SL7 containers

--- hive/gpvm_setup.sh
+++ hive/gpvm_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-source /grid/fermiapp/products/uboone/setup_uboone.sh
-echo "Setting up ninja v1_8_2"
+source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone_mcc9.sh
+
+echo "Setting up ninja v1_8_4"
 setup ninja v1_8_2
 echo "Setting up uboonecode v08_00_00_64"
 setup uboonecode v08_00_00_64 -qe17:prof
@@ -9,10 +10,12 @@ echo "Setting up cmake v3_14_3"
 setup cmake v3_14_3
 echo "Setting up gcc v7_3_0"
 setup gcc v7_3_0
+setup root v6_12_04e -q e15:prof
 echo "Setting up GSL"
 setup gsl v2_4 -q prof
 echo "Setting up Eigen"
 setup eigen v3_3_4a;
+setup boost v1_66_0 -q e15:prof
 
  
 # HIVEDIR enviromental varaible is used to setup template xmls by bdt_covar class
